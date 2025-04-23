637b3d6a630763e6d23d51a1e672ce0013320428
Nguyen Ngoc Truc Quynh <nguyenngoctrucquynh2005@gmail.com>
2025-04-24 02:13:51 +0700

    DongHo finished

--- src/App.js
+++ src/App.js
@@ -8,43 +8,55 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import NotFoundPage from './pages/NotFoundPage';
-import CuChiGameG1 from './pages/CuChiGame/CuChiGameG1';
-import Morse from './pages/CuChiGame/Morse';
 import DongHoGame from './pages/DongHoGame/DongHoGameG1';
+
 import FlipCard from './pages/DongHoGame/FlipCard';
 
 function App() {
   const { loading } = useAuth();
 
+  // if (loading) {
+  //   return <LoadingScreen />;
+  // }
 
   return (
     <div className="app">
       {/* <Header /> */}
       <main className="main-content">
         <Routes>
-
-           {/* /*<Route path="/" element={<FlipCard />} />* */}
-          {/* <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/RegisterPage" element={<RegisterPage />} /> */}
-
-           <Route path="/" element={<HomePage />} /> 
-          {/* <Route path="/LoginPage" element={<LoginPage />} /> */}
-          {/* <Route path="/RegisterPage" element={<RegisterPage />} /> */}
-
-          {/* path="/CuChiGame" sau khi test xong */}
-{/*           
-          <Route path="/" element={<CuChiGameG1/>} /> */}
-          {/* <Route path="/morse" element={<Morse />} /> */}
-
-           <Route path="/museum" element={<MuseumPage />}></Route>
-{/*       
-          // <Route path="/profile" element={<ProfilePage />} />
-          // <Route path="*" element={<NotFoundPage />} />  */}
+           {/* <Route path="/" element={<HomePage />} /> */}
+         {/* <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/RegisterPage" element={<RegisterPage />} />
+           } />
+     
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />  */}
+           {/* 
+            */}
+           {/* <Route path="/museum" element={<MuseumPage />}/> */}
+           <Route path="/" element={<DongHoGame />} />
+           <Route path="/FlipCard" element={<FlipCard />} />
         </Routes>
       </main>
       {/* <Footer /> */}
     </div>
- 
+    // <Router>
+    //   <div className="app">
+    //     <main className="main-content">
+    //       <Routes>
+    //         <Route path="/" element={<HomePage />} />
+    //         {/* <Route path="/museum" element={<MuseumPage />} />
+    //         <Route path="/collection" element={<CollectionPage />} />
+    //         <Route path="/leaderboard" element={<LeaderboardPage />} /> */}
+    //         <Route path="/login" element={<LoginPage />} />
+    //         <Route path="/register" element={<RegisterPage />} />
+    //         {/* <Route path="/profile" element={<ProfilePage />} /> */}
+    //         <Route path="*" element={<NotFoundPage />} />
+    //       </Routes>
+    //     </main>
+    //   </div>
+    // </Router>
+    
   );
 }
 
