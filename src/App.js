cba4cffb24463dd71b4e72aecd8d060f09a52bf0
phuocan <anp250506@gmail.com>
2025-04-24 14:53:27 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
         <Routes>
 
 
-        {<Route path="/" element={<DongHoGame />} />}
-        {<Route path="/FlipCard" element={<FlipCard />} />}
+        {/* {<Route path="/" element={<DongHoGame />} />}
+        {<Route path="/FlipCard" element={<FlipCard />} />} */}
 
 
           <Route path="/" element={<HomePage />} />
