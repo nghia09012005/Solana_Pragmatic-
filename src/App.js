a0f419ab836bdf005e687689ecd00309b7515b3e
Nghia Tran <nghiatran912005@gmail.com>
2025-04-24 14:10:02 +0700

    fetch with be

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useAuth } from './hooks/useAuth';
+// import { useAuth } from './hooks/useAuth';
 import HomePage from './pages/HomePage';
 import MuseumPage from './pages/MuseumPage';
 // import CollectionPage from './pages/CollectionPage';
 // import LeaderboardPage from './pages/LeaderboardPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ProfilePage from './pages/ProfilePage';
-import NotFoundPage from './pages/NotFoundPage';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
+// import ProfilePage from './pages/ProfilePage';
+// import NotFoundPage from './pages/NotFoundPage';
 
 import DongHoGame from './pages/DongHoGame/DongHoGameG1';
 
@@ -21,7 +21,7 @@ import ThongTin304 from './pages/Thongnhat/ThongTin304';
 
 
 function App() {
-  const { loading } = useAuth();
+  // const { loading } = useAuth();
 
   // if (loading) {
   //   return <LoadingScreen />;
@@ -35,19 +35,15 @@ function App() {
 
 
 
-         {<Route path="/" element={<DongHoGame />} />}
-        {<Route path="/FlipCard" element={<FlipCard />} />} 
+         {/* {<Route path="/" element={<DongHoGame />} />} */}
+        {/* {<Route path="/FlipCard" element={<FlipCard />} />}  */}
 
-          {/* <Route path="/" element={<HomePage />} /> */}
+          <Route path="/" element={<HomePage />} />
           {/* <Route path="/" element={<TankGame />} /> */}
 
           {/* <Route path="/" element={<Bantin />} /> */}
 
           {/* <Route path="/" element={<ThongTin304 />} /> */}
-
-
-          {/* <Route path="/LoginPage" element={<LoginPage />} />
-          <Route path="/RegisterPage" element={<RegisterPage />} /> */}
 
           {/* <Route path="/" element={<HomePage />} /> */}
           {/* <Route path="/LoginPage" element={<LoginPage />} /> */}
@@ -56,9 +52,9 @@ function App() {
           {/* path="/CuChiGame" sau khi test xong */}
           
           {/* <Route path="/" element={<CuChiGameG1/>} />
-          <Route path="/morse" element={<Morse />} />
+          <Route path="/morse" element={<Morse />} />*/}
 
-          <Route path="/museum" element={<MuseumPage />} /> */}
+          <Route path="/museum" element={<MuseumPage />} />
       
           {/* <Route path="/profile" element={<ProfilePage />} /> */}
          {/* <Route path="*" element={<NotFoundPage />} />  */}
