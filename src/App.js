536200264c9e273c1adee8152e94db6f64a97ef2
Nguyen Ngoc Truc Quynh <nguyenngoctrucquynh2005@gmail.com>
2025-04-26 12:18:12 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -36,36 +36,17 @@ function App() {
         <Routes>
 
           {/* <Route path="/" element={<HomePage />} /> */}
+{/* 
 
+          {/* <Route path="/museumpage" element={<MuseumPage />} />
 
-          <Route path="/" element={<HomePage />} />
-          <Route path="/museumpage" element={<MuseumPage />} />
-
-          <Route path="/tankgame" element={<TankGame />} />
-
-          {/* <Route path="/" element={<HomePage />} /> */}
-
-
-
+          <Route path="/tankgame" element={<TankGame />} /> */}
           
           {/* Tây Nguyên Gongs Game */}
 
-
-          {/* <Route path="/" element={<TayNguyenGame />} /> */}
-          {/* <Route path="/" element={<HomePage />} /> */}
-
-
           {/* <Route path="taynguyengame" element={<TayNguyenGame />} />
 
-
-          {/* <Route path="/" element={<HomePage />} /> */}
-
-          {/* <Route path="/" element={<HomePage />} /> */}
-
-          {/* <Route path="/" element={<Personalmuseum />} /> */}
-
           {/* <Route path="/bantin" element={<Bantin />} /> 
-
 
            <Route path="/" element={<ThongTin304 />} />
            
@@ -77,13 +58,11 @@ function App() {
 
           {/* path="/CuChiGame" sau khi test xong */}
                     
-          {/* <Route path="/cuchigame" element={<CuChiGameG1/>} />
-          <Route path="/morse" element={<Morse />} /> */}
+          <Route path="/" element={<CuChiGameG1/>} />
+          <Route path="/morse" element={<Morse />} />
 
           
           {/* <Route path="/bantin" element={<Bantin />} /> */}
-          {/* Cu Chi Game */}
-          
           
           {/* Dong Ho Game */}
           {/* <Route path="/" element={<DongHoGame />} />
@@ -92,7 +71,7 @@ function App() {
           {/* <Route path="/" element={<ProfilePage />} />  */}
           {/* <Route path="*" element={<NotFoundPage />} /> */}
 
-          <Route path="/museum" element={<PersonalMuseum />} /> 
+          {/* <Route path="/" element={<PersonalMuseum />} />  */}
 
         </Routes>
       </main>
